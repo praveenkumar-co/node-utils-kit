dc45d3cfb70f529fbda9b5940a79b3b20ba8f5af
PRAVEEN <praveen.iyu123@gmail.com>
2025-12-24 09:07:46 +0530

    fixed: gaurd Error.captureStackTrace for non-node environment

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -13,6 +13,8 @@ export class ApiError extends Error {
     this.statusCode = statusCode;
     this.errors = errors;
 
-    Error.captureStackTrace(this, this.constructor);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
